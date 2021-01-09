97e995b6de12aed9d1ba2f6f1db613144b2e02b3
anthony <y950727@gmail.com>
2021-01-09 15:53:37 +0800

    redis-springboot

--- redis-springboot/src/main/java/com/redisspringboot/User.java
+++ redis-springboot/src/main/java/com/redisspringboot/User.java
@@ -4,10 +4,25 @@ import lombok.Data;
 
 import java.io.Serializable;
 
-@Data
 public class User implements Serializable {
 
     private String name;
 
     private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
