b0bdd008fc73473e835e34bf2d4e80515bd34f8f
anthony <y950727@gmail.com>
2021-02-27 18:23:28 +0800

    x

---
+++ go-demo/errordemo/method.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// 定义一个 DivideError 结构
+type DivideError struct {
+	dividee int
+	divider int
+}
+
+// 实现 `error` 接口
+func (de *DivideError) Error() string {
+	strFormat := `
+    Cannot proceed, the divider is zero.
+    dividee: %d
+    divider: 0
+`
+	return fmt.Sprintf(strFormat, de.dividee)
+}
+
+func main() {
+
+
+
+}
---
+++ go-demo/errordemo/method2.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func Test() {
+fmt.Printf("12312312")
+
+}
